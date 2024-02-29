cb4676cd90a9e2aafff038b965fff45a9d331079
0xy-doxy <121454689+0xy-doxy@users.noreply.github.com>
2024-02-29 18:27:54 +0000

    function and scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,3 +29,45 @@ function loginUserMessage(username){
 
 //console.log(loginUserMessage("pratik"));
 console.log(loginUserMessage());
+
+
+function calculateCartaPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartaPrice(200,400,600,800,1000));
+
+function calculateCartaPrice2(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartaPrice2(200,400,600,800,1000));
+
+// passing object in function
+
+const user = {
+
+    username : "pratik",
+    price : 199
+}
+
+function handleObject(anyobject){
+
+     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price : 99
+})
+
+// passing array in function
+
+const myArray = [100, 200, 300, 400, 500]
+
+function returnSecondValue(anyarray){
+    return anyarray[1]
+}
+
+console.log(returnSecondValue(myArray));
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+if(true){
+
+    let a = 100
+    const b = 200
+    var c = 300
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);  // thats why var should not be used
