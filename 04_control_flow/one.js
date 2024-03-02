b6da3a0b9ea22c8e254338fc59096994092d5c05
0xy-doxy <121454689+0xy-doxy@users.noreply.github.com>
2024-03-02 15:19:53 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+// if-else
+const isUserLoggedIn = true
+const temp = 35
+
+// if(temp < 50){
+//     console.log("less than 50");
+// } else { 
+//     console.log("greater than 50");
+// }
+
+// implicit scope 
+const score = 1000
+
+// if(score > 500) console.log("high");
+
+// else-if
+
+const balance = 1000
+
+// if(balance < 500){
+//     console.log("less tha 500");
+// } else if (balance < 750){
+//     console.log("less tha 500");
+
+// } else if (balance < 750){
+//     console.log("less tha 500");
+
+// } else {
+//     console.log("less than 1200");
+// }
+
+
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,22 @@
+// switch
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("nothing");
+        break;
+}
