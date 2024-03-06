b2b109587f475929c9f1acd4dc0802adf6398a8e
0xy-doxy <121454689+0xy-doxy@users.noreply.github.com>
2024-03-06 02:18:59 +0000

    for each

--- 05_itrerations/five.js
+++ 05_itrerations/five.js
@@ -11,10 +11,33 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 //     console.log(item);
 // } )
 
-function printme(item) {
-    console.log(item);
-}
+// function printme(item) {
+//     console.log(item);
+// }
 
-coding.forEach(printme)
+// coding.forEach(printme)
 
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "java",
+        languageFileName: "jv"
+    },
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+} )
 
