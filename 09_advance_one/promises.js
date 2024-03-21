41cbffbc361f3bf02b6eac26c5df508bec0ce939
0xy-doxy <121454689+0xy-doxy@users.noreply.github.com>
2024-03-21 03:01:35 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -24,3 +24,57 @@ const promiseTwo = new Promise(function(resolve, reject){
 }).then(function () {
     console.log("Async 2 resolved!");
 })
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        resolve({username :"chai", email : "chai@ex.com"})
+    }, 1000);
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve({username : "pratik", pass : "456"})
+        } else {
+            reject("ERROE : Something went wrong !")
+        }
+    }, 1000);
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch((error) => {
+    console.log(error);
+}).finally(() => {
+    console.log("The promise is either resolved or rejected.");
+})
+
+const promiseFive = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve({username : "Javascript", pass : "456"})
+        } else {
+            reject("ERROE : JS went wrong !")
+        }
+    }, 1000);
+})
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
