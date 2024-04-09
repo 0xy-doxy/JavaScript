903dcaacfe0182e7474711f8eb0e28e25b7221ba
0xy-doxy <121454689+0xy-doxy@users.noreply.github.com>
2024-04-09 05:35:15 +0000

    advace obj

---
+++ 10_oop/mathpi.js
@@ -0,0 +1,33 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: 'chai',
+    price: 200,
+    isAvailable: true,
+
+    orderChai : function(){
+        console.log("chai nahi bani!");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai , 'name',{
+    //writable :false,
+    enumerable: false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (const [key,value] of Object.entries(chai)) {
+    
+        if (typeof value !== 'function') {
+            console.log(`${key} : ${value}`);
+        }
+}
